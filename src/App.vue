3fb3a2ee84b9466b69d00509e22963f3b14255f4
linus <linus.hjeltman@hotmail.com>
2022-07-24 12:48:12 +0200

    v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <img alt="Vue logo" src="./assets/128.jpg">
+  <div id="app">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+      integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <HelloWorld/>
+    <ContactInformation />
+    <meta name="theme-color" content="#243b55"/>
+    <!-- <img alt="Vue logo" src="./assets/128.jpg"> -->
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ContactInformation from './components/ContactInformation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ContactInformation
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f3f3f3;
   margin-top: 60px;
 }
-img{
+
+img {
   width: 100%;
+}
+
+body {
+  background-image: url('@/assets/128.jpg');
+  background-position: top;
+  background-position-y: 14%;
+  background-blend-mode: screen;
+}
+
+@media only screen and (max-width: 400px) {
+  body {
+    background-image: url('@/assets/128.jpg');
+    background-position-y: 9%;
+    background-size: 150%;
+
+  }
 }
 </style>
---
+++ src/components/ContactInformation.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- <div class="mail">
+        <p>linus.hjeltman@hotmail.com</p>
+    </div> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <div class="contact">
+        <a href="https://github.com/linushjeltman">
+            <p> <i class="fab fa-github-square"></i> linushjeltman </p>
+        </a>
+        <a href="tel:0705181455">
+            <p> <i class="fas fa-phone-square"></i> 070 518 14 55 </p>
+        </a>
+        <a href="mailto:linus.hjeltman@hotmail.com">
+            <p> <i class="fas fa-envelope-square"></i> linus.hjeltman@hotmail.com</p>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.mail {
+    font-size: 15px;
+}
+
+a:link,
+a:visited {
+    color: white;
+    text-decoration: none;
+}
+
+a:hover,
+a:active {
+    color: rgba(255, 255, 255, 0.753);
+}
+
+.contact {
+    text-align: left;
+    font-size: 30px;
+    position: absolute;
+    bottom: 0;
+}
+
+@media only screen and (max-width: 400px) {
+    .contact {
+        font-size: 20px;
+    }
+}
+</style>
