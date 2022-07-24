8078b04e9c2f4bd31d9eb9dc73cfdf204f98bd06
linus <linus.hjeltman@hotmail.com>
2022-07-24 10:00:40 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/128.jpg">
 </template>
 
 <script>
@@ -22,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  width: 100%;
 }
 </style>
